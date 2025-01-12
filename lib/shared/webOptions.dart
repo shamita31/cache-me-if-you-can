73eba732256dcdcabf5e618993b65e76dc288f1a
KartikB3 <167681395+KartikB3@users.noreply.github.com>
2025-01-12 23:15:13 +0530

    Update webOptions.dart

--- lib/shared/webOptions.dart
+++ lib/shared/webOptions.dart
@@ -1,5 +1,5 @@
 class WebOptions {
-  static String apiKey = "AIzaSyCSnHnjsevGAAdX4Bc-EAZYyTN6BUa7YIk";
+  static String apiKey = "";
   static String apiId = "1:309590365896:web:3431d766afdd1d0f0437fd";
   static String messagingSenderId = "309590365896";
   static String projectId = "pawcrastinot";
